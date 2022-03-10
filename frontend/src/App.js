7ec4905cdc6dba4da556c1e9a834627359a12943
Alvaro <sanchez99esp@gmail.com>
2022-03-10 09:56:49 +0100

    First Version

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { About, Header, Footer, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      {/* <Testimonial /> */}
+      <Footer />
+    </div>
+  );
+};
---
+++ frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsLinkedin, BsGithub} from 'react-icons/bs'
+
+const SocialMedia = () => (
+  <div className="app__social">
+    
+      <a href='https://www.linkedin.com/in/%C3%A1lvaro-rueda-s%C3%A1nchez-6812511a0/' target='_blank' rel="noopener noreferrer"> <div><BsLinkedin /></div></a>
+    
+    
+    <a href='https://github.com/alvarodachez' target='_blank' rel="noopener noreferrer"> <div><BsGithub /></div></a>
+      
+
+  </div>
+);
+
+export default SocialMedia;
